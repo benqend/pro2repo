eae1e789f3e891fa026331fdf9a17ef2a4d82ec8
benqend <benqend@163.com>
2012-05-25 15:19:48 +0800

    1 commit

---
+++ src/cn/leo/test/TestBean.java
@@ -0,0 +1,13 @@
+package cn.leo.test;
+
+public class TestBean {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaa");
+	}
+
+}
